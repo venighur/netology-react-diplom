f99b94ab319aebe9ededfb55b42a3fe08aa51fba
ProkopenkoAV <arty1987@mail.ru>
2024-07-10 17:29:50 +0300

    реализованы статичные страницы

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <section className="top-sales">
+      <h2 className="text-center">Страница не найдена</h2>
+      <p>
+        Извините, такая страница не найдена!
+      </p>
+    </section>
+  );
+}
+
+export default NotFound;
