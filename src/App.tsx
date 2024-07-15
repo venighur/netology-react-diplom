d518c178fa8d790824ff1a8a89ee6378e0d98dcb
ProkopenkoAV <arty1987@mail.ru>
2024-07-15 12:02:55 +0300

    реализована базовая структура приложения и роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// pages
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import About from './pages/About';
+import Product from './pages/Product';
+import Contacts from './pages/Contacts';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
+// components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Banner from './components/Banner';
+// styles
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <main className="container">
+          <div className="row">
+            <div className="col">
+              <Banner />
+              <Routes>
+                <Route path="/" index element={<Home />}/>
+                <Route path="/catalog" element={<Catalog withSearch />}/>
+                <Route path="/about" element={<About />}/>
+                <Route path="/contacts" element={<Contacts />}/>
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/catalog/:id" element={<Product />} />
+                <Route path="*" element={<NotFound />}/>
+              </Routes>
+            </div>
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
