501356cb32c909c01ff8eacae4b31cdc60fb7a4e
ProkopenkoAV <arty1987@mail.ru>
2024-07-15 12:01:51 +0300

    реализовано отображение виджета корзины в зависимости от количества добавленных товаров, реализован виджет строки поиска

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate, NavLink } from 'react-router-dom';
+import { useAppSelector } from '../app/hooks';
+import { selectCount } from '../features/cartSlice';
 import logo from '../img/header-logo.png';
 
 function Header() {
   const navigate = useNavigate();
+  const count = useAppSelector(selectCount);
+
+  const [searchValue, setSearchValue] = useState('');
+  const [showSearchForm, setShowSearchForm] =  useState(false);
 
   const setActive = (isActive: boolean) => {
     return `nav-link ${isActive ? 'active' : ''}`;
+  }
+
+  const searchClickHandler = () => {
+    if (!showSearchForm) {
+      setShowSearchForm(true);
+    } else {
+      searchValue ? navigate('/catalog', { state: searchValue }) : setShowSearchForm(false);
+    }
   }
 
   return (
@@ -34,10 +48,21 @@ function Header() {
               </ul>
               <div>
                 <div className="header-controls-pics">
-                  <div data-id="search-expander" className="header-controls-pic header-controls-search"></div>
+                  <form className="header-controls-search-form" style={{ display: showSearchForm ? 'block' : 'none' }}>
+                    <input
+                      className="form-control"
+                      placeholder="Поиск"
+                      value={searchValue}
+                      onChange={(e) => setSearchValue(e.target.value)}
+                    />
+                  </form>
+                  <div
+                    data-id="search-expander"
+                    className="header-controls-pic header-controls-search"
+                    onClick={searchClickHandler}></div>
                   <div className="header-controls-pic header-controls-cart" onClick={() => navigate('/cart')}>
-                    <div className={`header-controls-cart${localStorage.length && '-full'}`}>
-                      {localStorage.length > 0 ? localStorage.length : ''}
+                    <div className={`header-controls-cart${count && '-full'}`}>
+                      {count > 0 ? count : ''}
                     </div>
                     <div className="header-controls-cart-menu"></div>
                   </div>
