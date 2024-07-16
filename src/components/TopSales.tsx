2feeb196bbbd2a4605a53dac21feafa199a0c234
ProkopenkoAV <arty1987@mail.ru>
2024-07-16 12:36:11 +0300

    реализован вывод сообщения об ошибке в хитах продаж

--- src/components/TopSales.tsx
+++ src/components/TopSales.tsx
@@ -2,32 +2,42 @@ import React, { useEffect, useState } from 'react';
 import { Item } from './catalog';
 import { CatalogItemProps } from '../types';
 import Preloader from './Preloader';
+import Alert from './Alert';
 
 function TopSales() {
   const [topSales, setTopSales] = useState<CatalogItemProps[]>([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [show, setShow] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    setLoading(true);
     fetch(process.env.REACT_APP_TOP_SALES_URL as string)
       .then((response) => response.json())
       .then((data) => {
+        setError(false);
+        setShow(data.length > 0);
         setTopSales(data);
       })
-      .catch((error) => console.error(error));
+      .catch((error) => {
+        console.error(error);
+        setError(true);
+      });
 
     setLoading(false);
   }, []);
 
   return (
     <>
-      {!loading && topSales.length === 0 ? null : (
+      {!show ? null : (
         <section className="top-sales">
           <h2 className="text-center">Хиты продаж!</h2>
           {loading ? <Preloader /> : (
-            <div className="row">
-              {topSales.map((item) => <Item key={item.id} {...item} />)}
-            </div>
+            <>
+              {error && <Alert text="Ошибка при загрузке блока Хиты продаж" status="danger" />}
+              <div className="row">
+                {topSales.map((item) => <Item key={item.id} {...item} />)}
+              </div>
+            </>
           )}
         </section>
       )}
